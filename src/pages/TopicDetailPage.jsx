89b2261a7cff896a070fa36c0eae27e4489a7238
Jian <jian.media@gmail.com>
2024-06-21 10:26:54 +0100

    at topics page

--- src/pages/TopicDetailPage.jsx
+++ src/pages/TopicDetailPage.jsx
@@ -1,36 +1,59 @@
-// import React, { useEffect, useState } from "react";
-// import { useParams } from "react-router-dom";
-// import { getArticles } from "../services/articlesApi";
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import { getArticles } from "../services/articlesApi";
+import "./ArticleDetailPage.css"
 
 
-// export function TopicDetailPage() {
-//     const {topicSlug} = useParams()
-//     const [articles, setArticles] = useState([])
+ function TopicDetailPage() {
+    const {topicSlug} = useParams()
+    const [articles, setArticles] = useState([])
 
-//     useEffect(() => {
-//     getArticles(({topic: topicSlug}))
-//     .then((response) => {
-//         setArticles(response.data.articles)
-//     })
-//     .catch((error) => {
-//         console.error(`Error fetching ${topicSlug}`, error)
-//     })
-// }, [topicSlug])
+    useEffect(() => {
+    getArticles(({topic: topicSlug}))
+    .then((response) => {
+        setArticles(response.data.articles)
+    })
+    .catch((error) => {
+        console.error(`Error fetching ${topicSlug}`, error)
+    })
+}, [topicSlug])
 
-// return (
-//     <tbody>
-//       {articles.map((article) => (
-//         <tr key={article.article_id}>
-//           <td>
-//             <Link to={`/articles/${article.article_id}`}>{article.title}</Link>
-//           </td>
-//           <td>
-//             <img src={article.article_img_url} alt={article.title} />
-//           </td>
-//         </tr>
-//       ))}
-//     </tbody>
-//   );
+return (
+    <div className="topic">
+      <div className="list">
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Image</th>
+            </tr>
+          </thead>
+          
+        </table>
+      </div>
+    </div>
+  );
 
-// }
 
+return (
+    <div className="topics">
+        <h1>Articles in {topicSlug}</h1>
+      <tbody>
+      {articles.map((article) => (
+        <tr key={article.article_id}>
+          <td>
+            <Link to={`/articles/${article.article_id}`}>{article.title}</Link>
+          </td>
+          <td>
+            <img src={article.article_img_url} alt={article.title} />
+          </td>
+        </tr>
+      ))}
+    </tbody>  
+    </div>
+    
+  );
+
+}
+
+export default TopicDetailPage
